866dfd538917d6027d963293620f10cfb24123b6
morriswr01 <morriswr01@gmail.com>
2019-10-11 10:26:35 +0100

    Initial Backend Setup. Basic packages installed and server running

---
+++ server.js
@@ -0,0 +1,21 @@
+// Module dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+
+// Initialise the express app
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Connect to mongoDB / APIs
+// ...
+
+// Routes
+
+// Start server and listen on port 5000
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
