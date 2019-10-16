4d6e9e14e8685064e74f94ba54e24185682110ed
morriswr01 <morriswr01@gmail.com>
2019-10-16 18:13:54 +0100

    Added basic hello world front end react using create react app

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1
+					className="App-link"
+				>
+					Pins
+        		</h1>
+			</header>
+		</div>
+	);
+}
+
+export default App;
