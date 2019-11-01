fe270fab9843739973da480b323c389a037f7ed4
Will Morris <morriswr01@gmail.com>
2019-11-01 15:44:04 +0000

    Done buttons for login and registration on search screen

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,30 +1,28 @@
 import React from "react";
 import { Button } from "reactstrap";
 
-// scss
+// SCSS
 import "../assets/stylesheets/index.scss";
 
 export default function Search() {
     return (
         <div>
-            <div className='App grid-container'>
-                {/* Header of search screen */}
-                <header className='search-header'>
-                    {/* Login and registration buttons */}
-                    <div className="btnContainer">
-                        <Button color="primary"> Login </Button>
-                        <Button color="danger"> Register </Button>
-                    </div>
-                    {/* Title and subheading */}
+            {/* Login and registration buttons */}
+            <div className='nav-container clearfix'>
+                {/* Login and registration buttons */}
+                <Button color='primary'> Login </Button>
+                <Button color='danger'> Register </Button>
+            </div>
+            {/* Title and subheading */}
+            <header className='search-header'>
+                <div className='search-heading'>
                     <h1 className='search-title'>Pins</h1>
                     <h3 className='search-subtitle'>
                         &lt; Some Catchy Title &gt;
                     </h3>
-                </header>
-
-
-                {/* Search Function and filtering */}
-            </div>
+                </div>
+            </header>
+            {/* Search Function and filtering */}
         </div>
     );
 }
