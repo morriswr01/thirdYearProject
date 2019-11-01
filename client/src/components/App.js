6b0b2044259edba79292bfda485ca42ad5fb1cae
morriswr01 <morriswr01@gmail.com>
2019-11-01 12:10:11 +0000

    Changes some file structure

---
+++ client/src/components/App.js
@@ -0,0 +1,17 @@
+// Module Imports
+import React from 'react';
+
+// Component Imports
+import Search from './Search'
+
+// scss
+import '../assets/stylesheets/index.scss';
+
+function App() {
+	return (
+		
+		<Search />
+	);
+}
+
+export default App;
