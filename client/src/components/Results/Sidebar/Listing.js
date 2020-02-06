5b1423262f1c78b81972ceb3d7b05ffafc7fd78b
morriswr01 <morriswr01@gmail.com>
2020-02-06 17:53:19 +0000

    Added heart button to listings

--- client/src/components/Results/Sidebar/Listing.js
+++ client/src/components/Results/Sidebar/Listing.js
@@ -2,52 +2,63 @@ import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+
 import { setSelectedListing } from "../../../actions/listingActions";
 
-class Listing extends React.Component {
-    render() {
-        const {
-            img_url,
-            price_formatted,
-            summary,
-            title,
-            bedroom_number
-        } = this.props.listing;
+function Listing(props) {
+    const {
+        img_url,
+        price_formatted,
+        summary,
+        title,
+        bedroom_number
+    } = props.listing;
 
-        return (
-            <div
-                className={
-                    this.props.listingNumber ===
-                    this.props.selectedListing.listingNumber
-                        ? "sidebarListing selected"
-                        : "sidebarListing"
-                }
-                onClick={() =>
-                    this.props.setSelectedListing({
-                        ...this.props.listing,
-                        listingNumber: this.props.listingNumber
-                    })
-                }
-            >
+    return (
+        <div
+            className={
+                props.listingNumber === props.selectedListing.listingNumber
+                    ? "sidebarListing selected"
+                    : "sidebarListing"
+            }
+            onClick={() =>
+                props.setSelectedListing({
+                    ...props.listing,
+                    listingNumber: props.listingNumber
+                })
+            }
+        >
+            <div className='sidebarListingInfo'>
                 <img src={img_url} alt='House' className='sidebarListingImg' />
-                <div className='sidebarListingInfo'>
-                    <div className='title'>
-                        <h3>{title}</h3>
-                    </div>
-                    <p className='summary'>{summary}</p>
-                    <p className='bedNum'>Bedrooms: {bedroom_number}</p>
-                    <div className='price'>
-                        <hr></hr>
-                        <h5>{price_formatted}</h5>
+                <div className='title'>
+                    <h3>{title}</h3>
+                </div>
+                <p className='summary'>{summary}</p>
+                <p className='bedNum'>Bedrooms: {bedroom_number}</p>
+                <div className='priceContainer'>
+                    <hr></hr>
+                    <div className='infoBar'>
+                        <h5 className='price'>{price_formatted}</h5>
+                        <span
+                            onClick={e => {
+                                console.log("Heart clicked");
+                                e.stopPropagation();
+                            }}
+                        >
+                            <FontAwesomeIcon className='heart' icon={faHeart} />
+                        </span>
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 Listing.propTypes = {
-    listing: PropTypes.object.isRequired
+    listing: PropTypes.object.isRequired,
+    favourite: PropTypes.bool
 };
 
 const mapStateToProps = state => ({
